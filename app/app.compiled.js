00b62e0c05e5fb6b425444f6bad7e048be0a2eb9
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2018-02-17 00:35:33 +0100

    Added a lot of content

--- app/app.compiled.js
+++ app/app.compiled.js
@@ -1,12 +1,385 @@
 'use strict';
 
-const Application = props => {
-    return React.createElement(
-        'div',
-        null,
-        'It is my resume'
-    );
+const get_years_since = date => get_full_years(date, new Date());
+
+const get_full_years = (start, end) => {
+    let years = end.getFullYear() - start.getFullYear();
+
+    if (end.getMonth() > start.getMonth()) {
+        return years;
+    } else if (end.getMonth() < start.getMonth()) {
+        return years - 1;
+    } else if (end.getDate() > start.getDate()) {
+        return years;
+    } else if (end.getDate() < start.getDate()) {
+        return years - 1;
+    } else {
+        return years;
+    }
 };
+
+const Application = () => React.createElement(
+    "main",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Artem Skoretskiy ",
+        React.createElement(
+            "span",
+            { className: "dense" },
+            "//"
+        ),
+        " ",
+        React.createElement(
+            "span",
+            { className: "highlight" },
+            "full-stack web developer"
+        )
+    ),
+    React.createElement(Services, null),
+    React.createElement(OtherServices, null),
+    React.createElement(Profile, null),
+    React.createElement(Skills, null),
+    React.createElement(Projects, null),
+    React.createElement(Timeline, null)
+);
+
+const Services = () => React.createElement(
+    "section",
+    null,
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Backend"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Frontend"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Cloud services integration"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "DevOps"
+        )
+    )
+);
+
+const OtherServices = () => React.createElement(
+    "section",
+    null,
+    React.createElement(
+        "h3",
+        null,
+        "More when needed"
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Technical lead"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Project manager"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Business analyst"
+        )
+    )
+);
+
+const Profile = () => React.createElement(
+    "section",
+    null,
+    React.createElement(
+        "h2",
+        null,
+        "Profile"
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            get_years_since(new Date(1981, 12, 7, 12)),
+            " years"
+        ),
+        React.createElement(
+            "li",
+            null,
+            get_years_since(new Date(2000, 9, 1, 12)),
+            " years as software developer"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Live in K\xF6ln"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Master of Economy"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Fluent English"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Intermediate German (B2.1)"
+        )
+    )
+);
+
+const Skills = () => React.createElement(
+    "section",
+    null,
+    React.createElement(
+        "h2",
+        null,
+        "Skills"
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Python 3"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Django"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Django REST framework"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "JSON REST API"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Javascript"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "ES8"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "React"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Redux"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Webpack 3"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Babel"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "React hot-reload"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "CSS"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "HTML5"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "SQL"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "MySQL"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "SQLite"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "PostgreSQL"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "BASH"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "AWS S3 (+ upload)"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "AWS EC2 (+ custom AMIs)"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "packer"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "AWS CloudFront"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "AWS Glacier"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "ffmpeg"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "HTML5 video"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "HLS"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "DASH"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Docker"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Docker for Mac"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Docker Compose"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "GitLab CI"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Linux"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Debian"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Alpine"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "MacOS"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "PyCharm"
+        )
+    )
+);
+
+const Projects = () => React.createElement(
+    "section",
+    null,
+    React.createElement(
+        "h2",
+        null,
+        "Projects"
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "TODO"
+        )
+    )
+);
+
+const Timeline = () => React.createElement(
+    "section",
+    null,
+    React.createElement(
+        "h2",
+        null,
+        "Timeline"
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "TODO"
+        )
+    )
+);
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(React.createElement(Application, null), document.getElementById('root'));
--- app/app.js
+++ app/app.js
@@ -1,12 +1,141 @@
 'use strict';
 
-const Application = props => {
-    return (
-        <div>It is my resume</div>
-    );
+const get_years_since = date => get_full_years(date, new Date());
+
+const get_full_years = (start, end) => {
+    let years = end.getFullYear() - start.getFullYear();
+
+    if (end.getMonth() > start.getMonth()) {
+        return years;
+    } else if (end.getMonth() < start.getMonth()) {
+        return years - 1;
+    } else if (end.getDate() > start.getDate()) {
+        return years;
+    } else if (end.getDate() < start.getDate()) {
+        return years - 1;
+    } else {
+        return years;
+    }
 };
 
 
+const Application = () => (
+    <main>
+        <h1>Artem Skoretskiy <span className="dense">//</span> <span className="highlight">full-stack web developer</span></h1>
+        <Services/>
+        <OtherServices/>
+        <Profile/>
+        <Skills/>
+        <Projects/>
+        <Timeline/>
+    </main>
+);
+
+
+const Services = () => (
+    <section>
+        <ul>
+            <li>Backend</li>
+            <li>Frontend</li>
+            <li>Cloud services integration</li>
+            <li>DevOps</li>
+        </ul>
+    </section>
+);
+
+
+const OtherServices = () => (
+    <section>
+        <h3>More when needed</h3>
+        <ul>
+            <li>Technical lead</li>
+            <li>Project manager</li>
+            <li>Business analyst</li>
+        </ul>
+    </section>
+);
+
+
+const Profile = () => (
+    <section>
+        <h2>Profile</h2>
+        <ul>
+            <li>{get_years_since(new Date(1981, 12, 7, 12))} years</li>
+            <li>{get_years_since(new Date(2000, 9, 1, 12))} years as software developer</li>
+            <li>Live in Köln</li>
+            <li>Master of Economy</li>
+            <li>Fluent English</li>
+            <li>Intermediate German (B2.1)</li>
+        </ul>
+    </section>
+);
+
+
+const Skills = () => (
+    <section>
+        <h2>Skills</h2>
+        <ul>
+            <li>Python 3</li>
+            <li>Django</li>
+            <li>Django REST framework</li>
+            <li>JSON REST API</li>
+            <li>Javascript</li>
+            <li>ES8</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Webpack 3</li>
+            <li>Babel</li>
+            <li>React hot-reload</li>
+            <li>CSS</li>
+            <li>HTML5</li>
+            <li>SQL</li>
+            <li>MySQL</li>
+            <li>SQLite</li>
+            <li>PostgreSQL</li>
+            <li>BASH</li>
+            <li>AWS S3 (+ upload)</li>
+            <li>AWS EC2 (+ custom AMIs)</li>
+            <li>packer</li>
+            <li>AWS CloudFront</li>
+            <li>AWS Glacier</li>
+            <li>ffmpeg</li>
+            <li>HTML5 video</li>
+            <li>HLS</li>
+            <li>DASH</li>
+            <li>Docker</li>
+            <li>Docker for Mac</li>
+            <li>Docker Compose</li>
+            <li>GitLab CI</li>
+            <li>Linux</li>
+            <li>Debian</li>
+            <li>Alpine</li>
+            <li>MacOS</li>
+            <li>PyCharm</li>
+        </ul>
+    </section>
+);
+
+
+const Projects = () => (
+    <section>
+        <h2>Projects</h2>
+        <ul>
+            <li>TODO</li>
+        </ul>
+    </section>
+);
+
+
+const Timeline = () => (
+    <section>
+        <h2>Timeline</h2>
+        <ul>
+            <li>TODO</li>
+        </ul>
+    </section>
+);
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<Application />, document.getElementById('root'));
+    ReactDOM.render(<Application/>, document.getElementById('root'));
 });
